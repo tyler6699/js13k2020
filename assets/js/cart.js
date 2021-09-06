755ceef284bfd0f15774d5e6515c65f5b1b7ef7c
Ryan Tyler <ryan.tyler@southwales.ac.uk>
2021-09-07 00:38:25 +0100

    Move some code to hero class

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -3,7 +3,6 @@ function Cart() {
   this.cube = 16; // width of tiles
   this.scaled = this.scale*this.cube;
   this.hero = new hero(16, 16, canvasW/2, canvasH/2, 0, types.HERO, this.scale);
-  this.speed = 5;
   this.levels = []; // Array to get tiles surrounding an entity
   this.surTiles = [-1,1,18,19,20,-18,-19,-20];
   
@@ -25,105 +24,22 @@ function Cart() {
     
     // Controls
     if (left()){
-      this.hero.e.x -= this.gMove(-1,0);
+      this.hero.e.x -= this.hero.gMove(-1,0);
       this.hero.e.flip = true;
     }
     if (right()){
-      this.hero.e.x += this.gMove(1,0);
+      this.hero.e.x += this.hero.gMove(1,0);
       this.hero.e.flip = false;
     }
-    if (up())     this.hero.e.y -= this.gMove(0,-1);
-    if (down())   this.hero.e.y += this.gMove(0,1);
+    if (up())     this.hero.e.y -= this.hero.gMove(0,-1);
+    if (down())   this.hero.e.y += this.hero.gMove(0,1);
     if (space())  this.menu.curItm=actions.GUN;
     if(one()) cart.reset();
 
-    // Check for door hit
-    if(this.door != null){
-      this.hero.e.gun.bullets=[];
-      this.level = this.levels[this.door.loadRoom];
-      if(this.door.exitX != -1) this.hero.e.x = this.door.exitX;
-      if(this.door.exitY != -1) this.hero.e.y = this.door.exitY;
-      this.door = null;
-      introT = 32;
-    }
-    
-    // Set Hero Current Tile
-    heroRow = Math.floor((this.hero.e.y - this.hero.e.mhHScaled) / this.scaled);
-    heroCol = Math.floor((this.hero.e.x - this.hero.e.mhWScaled) / this.scaled);
-    heroTileIndex = heroCol + (19*heroRow);
-    if(this.currentTile != null) this.prevTile = this.currentTile;
-    this.currentTile = this.level.tiles[heroTileIndex];
-
-    if(this.currentTile != this.prevTile){
-      this.hero.e.colArr = [];
-      
-      // Add surrounding tiles
-      cart.surTiles.forEach(e => this.hero.e.colArr.push(this.level.tiles[heroTileIndex+e]));
-      this.level.mobs.forEach(e => this.hero.e.colArr.push(e.entity));
-    }
-
-    //GUN TEST
-    if(holdClick) holdClickT += delta;
-    if(processClick || holdClickT > .25){
-      ox = this.hero.e.x - this.hero.e.mhWScaled;
-      oy = this.hero.e.y - this.hero.e.mhHScaled;
-      dx = clickedAt.x;
-      dy = clickedAt.y;
-      
-      this.hero.e.gun.addBullets(ox,oy,dx,dy);
-    }
+    this.hero.checkDoor();
+    this.hero.setCurrentTile(this.scaled);
+    this.hero.checkGun();
         
-    // check for each pixel if the hero can move, starting with full amount
-    // The array contains tiles and mobs (Entities)
-    this.gMove = function(xx,yy){
-      rec = cloneRectanlge(this.hero.e.hb);
-      rec.x += xx * this.speed;
-      rec.y += yy * this.speed;
-      amount = this.speed;
-      stop=false;
-      canMove = true;
-      
-      // Move full amount and then try decreasing 
-      for(var i = this.speed; i>0; i--){
-        canMove = true;
-        
-        for (var t = 0; t < this.hero.e.colArr.length; t++) {
-          obj = this.hero.e.colArr[t];
-          
-          if(obj.isTile()){
-            if(!stop && rectColiding(obj.entity.hb,rec)){
-              if(obj.active && obj.entity.isSolid){
-                canMove = false;
-                break;
-              } else if(obj.isDoor && obj.doorSet()){  
-                this.door = obj.door;
-                stop = true;
-                break;
-              }
-              // Hurt Hero when on Oooze and moving
-              if(obj.entity.isBarrel()){
-                this.hero.e.hp-=.5;
-              }
-            }
-          } else { // MOB
-            if(!stop && obj.active && obj.isSolid && rectColiding(obj.hb, rec)){
-              canMove = false;
-              break;
-            }
-          }
-        }
-        if(canMove || stop){
-          break;
-        } else {
-          amount--;
-          rec.x -= xx;
-          rec.y -= yy;
-        }
-      }
-      
-      return amount;
-    }
-
     // Render
     renderStarField(time);
     this.level.draw(this.hero.e, delta);
--- assets/js/hero.js
+++ assets/js/hero.js
@@ -2,10 +2,105 @@ function hero(w, h, x, y, angle, type, scale) {
   this.e = new entity(w, h, x, y, angle, type, "", scale, 0, 0, false, 100);
   this.e.hp=100;
   this.e.gun = new Gun();
+  this.speed=5;
+  this.door=null;
+  this.currentTile=null;
   
   this.update = function(delta) {  
     this.time+=delta;
     this.e.gun.drawBullets(delta);
     this.e.update(delta);
   }  
+  
+  this.checkGun = function(){
+    if(holdClick) holdClickT += delta;
+    if(processClick || holdClickT > .25){
+      ox = this.e.x - this.e.mhWScaled;
+      oy = this.e.y - this.e.mhHScaled;
+      dx = clickedAt.x;
+      dy = clickedAt.y;
+      
+      this.e.gun.addBullets(ox,oy,dx,dy);
+    }
+  }
+  
+  this.checkDoor = function(){
+    // Check for door hit
+    if(this.door != null){
+      this.e.gun.bullets=[];
+      cart.level = cart.levels[this.door.loadRoom];
+      if(this.door.exitX != -1) this.e.x = this.door.exitX;
+      if(this.door.exitY != -1) this.e.y = this.door.exitY;
+      this.door = null;
+      cart.introT = 32;
+    }
+  }
+  
+  this.setCurrentTile = function(scaled){
+    // Set Hero Current Tile
+    heroRow = Math.floor((this.e.y - this.e.mhHScaled) / scaled);
+    heroCol = Math.floor((this.e.x - this.e.mhWScaled) / scaled);
+    heroTileIndex = heroCol + (19*heroRow);
+    if(this.currentTile != null) this.prevTile = this.currentTile;
+    this.currentTile = cart.level.tiles[heroTileIndex];
+
+    if(this.currentTile != this.prevTile){
+      this.e.colArr = [];
+      
+      // Add surrounding tiles
+      cart.surTiles.forEach(e => this.e.colArr.push(cart.level.tiles[heroTileIndex+e]));
+      cart.level.mobs.forEach(e => this.e.colArr.push(e.entity));
+    }
+  }
+  
+  // check for each pixel if the hero can move, starting with full amount
+  // The array contains tiles and mobs (Entities)
+  this.gMove = function(xx,yy){
+    rec = cloneRectanlge(this.e.hb);
+    rec.x += xx * this.speed;
+    rec.y += yy * this.speed;
+    amount = this.speed;
+    stop = false;
+    canMove = true;
+    
+    // Move full amount and then try decreasing 
+    for(var i = this.speed; i>0; i--){
+      canMove = true;
+      
+      for (var t = 0; t < this.e.colArr.length; t++) {
+        obj = this.e.colArr[t];
+        
+        if(obj.isTile()){
+          if(!stop && rectColiding(obj.entity.hb,rec)){
+            if(obj.active && obj.entity.isSolid){
+              canMove = false;
+              break;
+            } else if(obj.isDoor && obj.doorSet()){  
+              this.door = obj.door;
+              stop = true;
+              break;
+            }
+            // Hurt Hero when on Oooze and moving
+            if(obj.entity.isBarrel()){
+              this.e.hp-=.5;
+            }
+          }
+        } else { // MOB
+          if(!stop && obj.active && obj.isSolid && rectColiding(obj.hb, rec)){
+            canMove = false;
+            break;
+          }
+        }
+      }
+      if(canMove || stop){
+        break;
+      } else {
+        amount--;
+        rec.x -= xx;
+        rec.y -= yy;
+      }
+    }
+    
+    return amount;
+  }
 }
